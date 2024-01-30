47304f849908c28c589a13793db384fb16b1a790
Vikram9392 <vikramvallala12@gmail.com>
2024-01-30 14:13:59 +0530

    done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/product/:productId', (req, res) => {
+  res.json(req.params)
   //Write a code here for endpoint /product/:productId and also print parameter in json form
 });
 
